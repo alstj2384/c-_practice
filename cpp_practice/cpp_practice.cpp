6657a0649545fb11bda5ecbf14e876ad0358b52d
송민서 <aldtj2384@gmail.com>
2023-02-06 18:13:05 +0900

    Class Array

--- cpp_practice/cpp_practice.cpp
+++ cpp_practice/cpp_practice.cpp
@@ -2,9 +2,42 @@
 
 using namespace std;
 
+class MyCar {
+    private:
+        //멤버 변수
+        int fuel = 0;
+        bool power = false;
+
+    public:
+        //메소드
+        void go(){
+            this -> fuel--;
+        }
+
+        void oiling(int n){
+            this->fuel += n;
+        }
+
+        void fuel_check(){
+            cout << "연료 : " << fuel << endl;
+        }
+};
+
 int main()
 {
-    cout << "Hello!!!!" << endl;
+    MyCar car = MyCar();
+
+    //메서드 호출
+    car.oiling(100);
+    car.fuel_check();
+    
+    for(int i =0 ; i<10; i++) car.go();
+
+    car.fuel_check();
+    car.oiling(100);
+
+    for(int i=0;i<10;i++) car.go();
+    car.fuel_check();
 
     return 0;
 }
