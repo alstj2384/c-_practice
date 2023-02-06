dd70d263620d38dc5167371cd515507cc5fd8035
송민서 <aldtj2384@gmail.com>
2023-02-06 18:21:55 +0900

    Add Constructor and Destructor

--- cpp_practice/cpp_practice.cpp
+++ cpp_practice/cpp_practice.cpp
@@ -9,6 +9,21 @@ class MyCar {
         bool power = false;
 
     public:
+        //생성자
+        MyCar(){
+            this->fuel = 100;
+            this->power = true;
+        }
+        //생성자 다중 정의
+        MyCar(int n){
+            this->fuel = n;
+            this->power = true;
+        }
+
+        //소멸자(다중 정의 불가능)
+        ~MyCar(){
+            cout << "소멸되었습니다." << endl;
+        }
         //메소드
         void go(){
             this -> fuel--;
@@ -25,19 +40,36 @@ class MyCar {
 
 int main()
 {
-    MyCar car = MyCar();
+   
+    //메서드 호출
+    // MyCar car = MyCar();
+
+    // car.oiling(100);
+    // car.fuel_check();
+
+    // for(int i =0 ; i<10; i++) car.go();
+
+    // car.fuel_check();
+    // car.oiling(100);
+
+    // for(int i=0;i<10;i++) car.go();
+    // car.fuel_check();
+
+
+    //클래스 배열 사용
+    MyCar car[3]; //생성
+
+    for (int i = 0; i < 3; i++)car[i] = MyCar();
+    /*클래스 배열을 사용할 때는 생성 즉시 초기화가 되지 않으므로
+     * 생성 후 따로 for문을 돌려서 초기화 시켜주어야 함
+     * 기본 생성자가 클래스 내부에 존재할 경우 사용 가능
+    */
 
     //메서드 호출
-    car.oiling(100);
-    car.fuel_check();
-    
-    for(int i =0 ; i<10; i++) car.go();
-
-    car.fuel_check();
-    car.oiling(100);
-
-    for(int i=0;i<10;i++) car.go();
-    car.fuel_check();
+    car[0].oiling(100);
+    car[0].fuel_check();
+    for(int i = 0; i < 10; i++) car[0].go();
+    car[0].fuel_check();
 
     return 0;
 }
