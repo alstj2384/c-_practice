d98e619b41fdf14aecf270860f3146be91540464
송민서 <ass91160@naver.com>
2023-02-25 18:16:26 +0900

    2023-02-25

---
+++ cpp_practice/Baekjoon/Baekjoon2869.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int a, b ,v;
+    int h = 0;
+    int m = 0;
+    int cnt = 0;
+    cin >> a >> b >> v;
+
+    cout << v/(a-b)-b << endl;
+    cnt = v/a;
+    m = v%a;
+    m==0
+    
+    a-b*
+    if()
+    // while(true){
+    //     h += a;
+    //     cnt++;
+    //     if(h >= v){
+    //         cout << cnt << '\n';
+    //         return 0;
+    //     }
+    //     h -= b;
+        
+    // }
+    // if()
+
+    // h +a -b +a -b +a -b +a -b
+
+    // 2a-b >= v
+    // v-b
+
+    // 100 50
+    // 50 50 50 50 50 505050500505050505050
+
+    // 10억 / a-b 
+    return 0;
+}
